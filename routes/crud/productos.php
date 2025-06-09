7139f408b996b2a57f61ad8d100c9276f0a3ff90
Pusho.Dev <bethocubans1990@gmail.com>
2025-06-09 17:04:27 -0400

    Disponibilidad por user products vendor 🤯

--- routes/crud/productos.php
+++ routes/crud/productos.php
@@ -12,9 +12,5 @@ Route::middleware(['auth', 'verified'])->group(
 
         // Para las Ventas
         Route::resource('disponibles', ProductoVendedorController::class);
-
-        // Ruta específica para actualizar precio
-        Route::patch('/disponibles/{producto}', [ProductoVendedorController::class, 'update'])
-            ->name('disponibles.update');
     }
 );
