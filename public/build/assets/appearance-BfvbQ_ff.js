748658fccb97f3ed6b695c48eeef9c3d6b978018
Pusho.Dev <bethocubans1990@gmail.com>
2025-10-02 17:37:16 -0400

    Compiled Prorrateo

---
+++ public/build/assets/appearance-BfvbQ_ff.js
@@ -0,0 +1,16 @@
+import{u as m,j as e,L as d}from"./app-l0gf8Ezi.js";import{c as n}from"./utils-jAU0Cazi.js";import{c as a}from"./createLucideIcon-BA-j1afh.js";import{H as u}from"./heading-small-Cg2MIlfT.js";import{A as h}from"./app-layout-BwsEUsOZ.js";import{S as x}from"./layout-Dyvt4J2G.js";import"./index-43TG22rS.js";import"./index-DZZQifJx.js";import"./button-C1b7UJyB.js";import"./index-DbAubCAp.js";import"./Combination-JjW0NT3y.js";import"./index-D3Dpv9Xf.js";import"./index-CyG_Czgn.js";import"./app-logo-icon-BwUJy5l_.js";import"./separator-DJOa8DEb.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const y=[["rect",{width:"20",height:"14",x:"2",y:"3",rx:"2",key:"48i651"}],["line",{x1:"8",x2:"16",y1:"21",y2:"21",key:"1svkeh"}],["line",{x1:"12",x2:"12",y1:"17",y2:"21",key:"vw1qmm"}]],k=a("Monitor",y);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const g=[["path",{d:"M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z",key:"a7tn18"}]],f=a("Moon",g);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const b=[["circle",{cx:"12",cy:"12",r:"4",key:"4exip2"}],["path",{d:"M12 2v2",key:"tus03m"}],["path",{d:"M12 20v2",key:"1lh1kg"}],["path",{d:"m4.93 4.93 1.41 1.41",key:"149t6j"}],["path",{d:"m17.66 17.66 1.41 1.41",key:"ptbguv"}],["path",{d:"M2 12h2",key:"1t8f8n"}],["path",{d:"M20 12h2",key:"1q8mjw"}],["path",{d:"m6.34 17.66-1.41 1.41",key:"1m8zz5"}],["path",{d:"m19.07 4.93-1.41 1.41",key:"1shlcs"}]],j=a("Sun",b);function v({className:r="",...i}){const{appearance:o,updateAppearance:s}=m(),c=[{value:"light",icon:j,label:"Claro"},{value:"dark",icon:f,label:"Oscuro"},{value:"system",icon:k,label:"Configuración del Sistema"}];return e.jsx("div",{className:n("inline-flex gap-1 rounded-lg bg-neutral-100 p-1 dark:bg-neutral-800",r),...i,children:c.map(({value:t,icon:p,label:l})=>e.jsxs("button",{onClick:()=>s(t),className:n("flex items-center rounded-md px-3.5 py-1.5 transition-colors",o===t?"bg-white shadow-xs dark:bg-neutral-700 dark:text-neutral-100":"text-neutral-500 hover:bg-neutral-200/60 hover:text-black dark:text-neutral-400 dark:hover:bg-neutral-700/60"),children:[e.jsx(p,{className:"-ml-1 h-4 w-4"}),e.jsx("span",{className:"ml-1.5 text-sm",children:l})]},t))})}const A=[{title:"Appearance settings",href:"/settings/appearance"}];function R(){return e.jsxs(h,{breadcrumbs:A,children:[e.jsx(d,{title:"Configuración de Apariencia"}),e.jsx(x,{children:e.jsxs("div",{className:"space-y-6",children:[e.jsx(u,{title:"Configuración de Apariencia",description:"Actualizar su cuenta. Configuración de Apariencia"}),e.jsx(v,{})]})})]})}export{R as default};
--- public/build/assets/index-DhrH2apf.js
+++
@@ -1,1 +0,0 @@
-import{g as r,b as t}from"./app-e03D35Rb.js";var a=t();const e=r(a);export{e as R,a as r};
