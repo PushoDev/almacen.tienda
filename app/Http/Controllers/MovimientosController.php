8a2ab02311de40f466fac36afdcc01a342ba888a
Pusho.Dev <bethocubans1990@gmail.com>
2025-11-20 15:49:22 -0500

    Case 1 y 2 full

--- app/Http/Controllers/MovimientosController.php
+++ app/Http/Controllers/MovimientosController.php
@@ -311,18 +311,28 @@ class MovimientosController extends Controller
                         $almacenProductoDestino->update(['cantidad' => $nuevaCantidad]);
                     }
 
+                    // Al enviar, se incrementó cantidad_en_transito pero se mantuvo cantidad
+                    // Ahora, para completar el movimiento, debemos reducir la cantidad original en el origen
+                    AlmacenProducto::where([
+                        'almacen_id' => $movimiento->almacen_origen_id,
+                        'producto_id' => $detalle->producto_id
+                    ])->decrement('cantidad', $detalle->cantidad_despachada);
+
                     AlmacenProducto::where([
                         'almacen_id' => $movimiento->almacen_origen_id,
                         'producto_id' => $detalle->producto_id
                     ])->decrement('cantidad_en_transito', $detalle->cantidad_despachada);
 
                     if ($diferencia > 0) {
+                        // Si se reciben menos unidades de las despachadas, las diferencias se mantienen en el almacén de origen
+                        // Incrementamos el stock del emisor con las unidades no recibidas
                         AlmacenProducto::where([
                             'almacen_id' => $movimiento->almacen_origen_id,
                             'producto_id' => $detalle->producto_id
                         ])->increment('cantidad', $diferencia);
                         $totalDiferencias += $diferencia;
                     } elseif ($diferencia < 0) {
+                        // Si se reciben más unidades de las despachadas, se ajusta el stock del emisor
                         $diferenciaNegativa = abs($diferencia);
                         AlmacenProducto::where([
                             'almacen_id' => $movimiento->almacen_origen_id,
