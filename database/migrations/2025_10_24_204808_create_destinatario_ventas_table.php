81eaa14528240d31f83704085f967a54502621c8
Pusho.Dev <bethocubans1990@gmail.com>
2025-11-28 16:45:18 -0500

    table destinatario opcional

--- database/migrations/2025_10_24_204808_create_destinatario_ventas_table.php
+++ database/migrations/2025_10_24_204808_create_destinatario_ventas_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('nombre');
             $table->string('apellidos');
             $table->string('carnet_identidad')->unique();
-            $table->text('direccion_residencia');
+            $table->text('direccion_residencia')->nullable();
             $table->string('telefono_contacto')->nullable();
             $table->string('parentesco_cliente')->nullable(); // familiar, amigo, etc.
             $table->text('observaciones')->nullable();
