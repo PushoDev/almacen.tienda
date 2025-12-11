a8f25d6a762530156a61a798dcd0e93b1a07cd53
Pusho.Dev <bethocubans1990@gmail.com>
2025-12-11 14:49:12 -0500

    Modifiqued

---
+++ public/build/assets/input-error-oEzPCyGI.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-DOPIl8ZS.js";import{a as o}from"./createLucideIcon-B8Tiye4y.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
