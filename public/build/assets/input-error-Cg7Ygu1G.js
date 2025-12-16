eb56f7d418cf26694a6bebece68ee9b1ec4c311b
Pusho.Dev <bethocubans1990@gmail.com>
2025-12-16 10:50:34 -0500

    compiled

--- public/build/assets/input-error-Cg7Ygu1G.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-Zgf8Y98v.js";import{a as o}from"./createLucideIcon-o0F7SqWH.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
