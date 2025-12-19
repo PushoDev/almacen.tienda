bb7bfb48f3c671c68bf31fc36319b178c280ee43
Pusho.Dev <bethocubans1990@gmail.com>
2025-12-19 16:45:45 -0500

    main compiled

---
+++ public/build/assets/app-layout-CQTiP4ak.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-RDaHOz0l.js";import{A as s}from"./app-sidebar-layout-DvdxJRo0.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
