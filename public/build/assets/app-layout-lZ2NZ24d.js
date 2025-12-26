c33fe1325beb7a947a659f872d00e02aee16b4ce
Pusho.Dev <bethocubans1990@gmail.com>
2025-12-26 15:52:58 -0500

    compiled new

---
+++ public/build/assets/app-layout-lZ2NZ24d.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-B9KmdM8h.js";import{A as s}from"./app-sidebar-layout-CVEQwE3D.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
