a333d65735ae2e8ed980833ed719aea966a1d027
PushoDev <bethocubans1990@gmail.com>
2026-01-08 10:31:04 -0500

    updated data

--- public/build/assets/app-layout-B6YQ0tzc.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-DvTitAF6.js";import{A as s}from"./app-sidebar-layout-m0A8Eaeb.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
--- public/build/assets/input-error-DmajKc6X.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-DvTitAF6.js";import{a as o}from"./createLucideIcon-DaP9byW2.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
