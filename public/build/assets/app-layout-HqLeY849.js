561e9e3bad7396292dd39ce755ebd28e1c8fde90
PushoDev <bethocubans1990@gmail.com>
2026-01-12 13:46:43 -0500

    compiled cierres

---
+++ public/build/assets/app-layout-HqLeY849.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-BT8TR8Hz.js";import{A as s}from"./app-sidebar-layout-uzi8yASC.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
