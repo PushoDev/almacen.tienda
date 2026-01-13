604664d2eacaa2d7e7a66ca273806e581a73b07e
PushoDev <bethocubans1990@gmail.com>
2026-01-13 17:07:06 -0500

    deleted build products 403

--- public/build/assets/app-layout-CRydk8Zi.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-B9zDbEi2.js";import{A as s}from"./app-sidebar-layout-BLc6QEOr.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
