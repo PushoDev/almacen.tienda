4a32c3f605385d08e1be608e4e34e156182c76d6
PushoDev <bethocubans1990@gmail.com>
2026-01-13 17:10:14 -0500

    compiled 403 errors

---
+++ public/build/assets/app-layout-Dzj8jNth.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-Dxyn2xNM.js";import{A as s}from"./app-sidebar-layout-B-F-721n.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
---
+++ public/build/assets/input-error-BHwj-tVQ.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-Dxyn2xNM.js";import{a as o}from"./createLucideIcon-BcUJN9KB.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
