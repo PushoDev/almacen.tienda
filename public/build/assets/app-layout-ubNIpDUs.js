843626cff57e1bdfba63ab96c5f32366e857e002
PushoDev <bethocubans1990@gmail.com>
2026-01-14 13:17:59 -0500

    deleted compiled old

--- public/build/assets/app-layout-ubNIpDUs.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-_R1g5lyB.js";import{A as s}from"./app-sidebar-layout-D7lh0nn_.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
