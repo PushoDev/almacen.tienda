b42fb2039ac4a73f23485fd43966eeed1762f61f
PushoDev <bethocubans1990@gmail.com>
2026-01-16 10:50:35 -0500

    prepeing historial trasacciones

---
+++ resources/js/pages/Transacciones/Create.tsx
@@ -0,0 +1,84 @@
+
+import HeadingSmall from '@/components/heading-small';
+import { Button } from '@/components/ui/button';
+import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
+import { ScrollProgress } from '@/components/ui/scroll';
+import { Separator } from '@/components/ui/separator';
+import AppLayout from '@/layouts/app-layout';
+import { type BreadcrumbItem } from '@/types';
+import { Head, Link } from '@inertiajs/react';
+import { Banknote, FileText, Sheet } from 'lucide-react';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Resumen General',
+        href: '/dashboard',
+    },
+    {
+        title: 'Cuentas Monetarias',
+        href: '/cuentas',
+    },
+    {
+        title: 'Productos',
+        href: '/listado-productos',
+    },
+    {
+        title: 'Transacciones',
+        href: '#',
+    },
+];
+
+export default function NuevaTransacciones() {
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Historial" />
+            <ScrollProgress />
+            <div className="animate__animated animate__fadeIn flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+                <div className="bg-sidebar border-sidebar-accent animate__animated animate__fadeIn relative col-span-4 space-y-1 overflow-hidden rounded-2xl border border-dashed p-4">
+                    <HeadingSmall
+                        title="Transacciones"
+                        description="Administre las transacciones financieras, distribución de costos y movimientos entre cuentas, clientes y proveedores"
+                    />
+                    <Banknote
+                        size={70}
+                        color="#d6d3d1"
+                        className="pointer-events-none absolute right-2 bottom-0 translate-x-0 translate-y-[-5] transform animate-pulse opacity-40"
+                    />
+                </div>
+                <Separator className="col-span-4" />
+                {/* Acciones */}
+                <div className="flex justify-end gap-2">
+                    {/* Botón Editar */}
+                    <Link href="#">
+                        <Button variant="outline" className="hover:bg-chart-5 flex cursor-pointer items-center gap-2">
+                            <FileText size={16} />
+                            Exportar PDF
+                        </Button>
+                    </Link>
+
+                    {/* Botón Regresar */}
+                    <Link href="#">
+                        <Button variant="secondary" className="hover:bg-chart-2 flex cursor-pointer items-center gap-2">
+                            <Sheet size={16} />
+                            Exportar Excel
+                        </Button>
+                    </Link>
+                </div>
+                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
+                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    </div>
+                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
+                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    </div>
+                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
+                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    </div>
+                </div>
+                <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
+                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                </div>
+            </div>
+        </AppLayout>
+    );
+}
