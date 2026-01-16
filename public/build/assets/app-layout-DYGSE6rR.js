9a7bd5a6c3b14f36ca77819bb16864a929f3fb3e
PushoDev <bethocubans1990@gmail.com>
2026-01-16 18:07:35 -0500

    compiled cierres errors

---
+++ public/build/assets/app-layout-DYGSE6rR.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-UbNuDmWZ.js";import{A as s}from"./app-sidebar-layout-DsThZIge.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
