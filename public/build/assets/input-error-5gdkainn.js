8bdfbe5d9a4dc0c917c5c778592a3edd8780cee7
PushoDev <bethocubans1990@gmail.com>
2026-01-29 17:15:26 -0500

    details vendor show

---
+++ public/build/assets/input-error-5gdkainn.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-BFnDpFq8.js";import{a as o}from"./createLucideIcon-B1d372MT.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
