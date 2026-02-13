d37e08da9c02c25d909deb42db4d1eb1350e8641
PushoDev <bethocubans1990@gmail.com>
2026-02-13 15:04:45 -0500

    deleted build main 13/2

--- public/build/assets/app-layout-DJ7GbONJ.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-DvI2E_8I.js";import{l as s}from"./app-sidebar-layout-WasX37aT.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
