f4996df884b96d07eb090dca33f5292877beeaea
PushoDev <bethocubans1990@gmail.com>
2026-02-25 18:07:45 -0500

    compiler

--- public/build/assets/app-layout-DWxvexoq.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-CM2cdvQZ.js";import{l as s}from"./app-sidebar-layout-SsfZpjbs.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
