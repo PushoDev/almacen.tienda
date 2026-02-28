df415dcbb4e8bc4f07a1e28b0c036f56b8012beb
PushoDev <bethocubans1990@gmail.com>
2026-02-28 10:03:43 -0500

    build 28/2

--- public/build/assets/app-layout-ByxwqauV.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-CvmCAgVo.js";import{l as s}from"./app-sidebar-layout-nugHijRE.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
