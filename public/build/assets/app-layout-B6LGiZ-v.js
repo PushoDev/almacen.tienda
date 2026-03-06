a4e73f4b7ca9f5f1cc8f644a4abfbe9f8a36babb
PushoDev <bethocubans1990@gmail.com>
2026-03-06 14:01:32 -0500

    preparar rama para cierres optimizados

--- public/build/assets/app-layout-B6LGiZ-v.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-A5IdTVIi.js";import{l as s}from"./app-sidebar-layout-DUkm_au2.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
--- public/build/assets/input-error-D3MuU4g2.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-A5IdTVIi.js";import{a as o}from"./createLucideIcon-Bua3KkQe.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
