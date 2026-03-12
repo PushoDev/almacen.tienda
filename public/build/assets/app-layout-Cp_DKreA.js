c40a376103ee9b1cd203066ae25c06e3b03520cd
PushoDev <bethocubans1990@gmail.com>
2026-03-11 20:45:22 -0400

    updated 11/3

---
+++ public/build/assets/app-layout-Cp_DKreA.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-9l_SYNk6.js";import{k as s}from"./app-sidebar-layout-kfROfZoc.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
