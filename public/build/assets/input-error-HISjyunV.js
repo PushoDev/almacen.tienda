248bccfc8f3413a92103ed841827ab824300fd49
PushoDev <bethocubans1990@gmail.com>
2026-03-24 14:41:22 -0400

    listo casi casi

---
+++ public/build/assets/input-error-HISjyunV.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-BOWfN3nK.js";import{a as o}from"./createLucideIcon-CYMttbfC.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
