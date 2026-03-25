b01b0ec765edf3eb26cf49275fa6af297c43df9f
PushoDev <bethocubans1990@gmail.com>
2026-03-25 17:33:05 -0400

    listo para api publica ok

--- public/build/assets/app-layout-CReZAj7P.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-BOWfN3nK.js";import{k as s}from"./app-sidebar-layout-78g79_A7.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
