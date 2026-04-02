22dfdfd5d4042e2011d2131edbeaddfda941c98b
PushoDev <bethocubans1990@gmail.com>
2026-04-02 17:04:47 -0400

    compiled 2/4

--- public/build/assets/app-layout-Dp-HpoSr.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-DOn1ciGN.js";import{k as s}from"./app-sidebar-layout-zgFs7-Cq.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
