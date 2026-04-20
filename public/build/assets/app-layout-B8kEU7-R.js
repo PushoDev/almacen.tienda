e7f9e22cf2473f4969c7de92527a428eb4d141ee
PushoDev <bethocubans1990@gmail.com>
2026-04-20 16:35:38 -0400

    inicial arrglos de cuentas proveedores - clientes

--- public/build/assets/app-layout-B8kEU7-R.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-BbSwEhVg.js";import{k as s}from"./app-sidebar-layout-CVSORpZF.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
