16a9d15c35072fc59876384b56b9eb3c2b73e9ff
PushoDev <bethocubans1990@gmail.com>
2026-04-28 17:26:30 -0400

    compiled productions 26/04

--- public/build/assets/app-layout-DWZMgcQP.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-CbldRK-F.js";import{k as s}from"./app-sidebar-layout-Dct_R67L.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
