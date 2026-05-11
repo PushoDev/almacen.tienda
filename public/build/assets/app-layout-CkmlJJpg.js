bc3cdeb54fc4443f27fd6e328b57254c8592788c
PushoDev <bethocubans1990@gmail.com>
2026-05-11 16:36:30 -0400

    Compiled 11/5

--- public/build/assets/app-layout-CkmlJJpg.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-CR88MToY.js";import{k as s}from"./app-sidebar-layout-CVT3nQBt.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
