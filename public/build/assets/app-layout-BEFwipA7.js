96684240e0ed8c52e08fa4f032af1c5a207223d2
PushoDev <bethocubans1990@gmail.com>
2026-05-15 17:02:24 -0400

    compiled 15/5

---
+++ public/build/assets/app-layout-BEFwipA7.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-DfsQr_60.js";import{k as s}from"./app-sidebar-layout-BF1nrTmK.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
--- public/build/assets/app-layout-CbXYjh-l.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-CZMcxejq.js";import{k as s}from"./app-sidebar-layout-qnh53OwK.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
