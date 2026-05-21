20d14a89ecce7ac8b1090579825e5d6debe24a66
PushoDev <bethocubans1990@gmail.com>
2026-05-21 18:18:36 -0400

    Compiled 21/05

---
+++ public/build/assets/app-layout-DXsW1V58.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-BLmssYp0.js";import{k as s}from"./app-sidebar-layout-VaQ0ITTY.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
