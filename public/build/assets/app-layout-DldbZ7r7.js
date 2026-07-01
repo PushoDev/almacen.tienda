c07ac6862606c4e6a1ba8a13f99e40db1607aaae
PushoDev <bethocubans1990@gmail.com>
2026-07-01 15:25:27 -0400

    Updated Production 1-Julio

---
+++ public/build/assets/app-layout-DldbZ7r7.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-CPdU03Rg.js";import{y as s}from"./app-sidebar-layout-BkRzi9rA.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
