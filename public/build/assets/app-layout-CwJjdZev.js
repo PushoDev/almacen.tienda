7ee9bfafffe86a2e7174db22e45af5f0fa64f034
PushoDev <bethocubans1990@gmail.com>
2026-07-24 13:54:44 -0400

    Compiled 24-julio

---
+++ public/build/assets/app-layout-CwJjdZev.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-xrVR639Y.js";import{y as s}from"./app-sidebar-layout-BgUQkApq.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
---
+++ public/build/assets/react-0_UQZIUZ.js
@@ -0,0 +1,1 @@
+import{m as o}from"./proxy-Cqxkl8PX.js";const t=o;export{t as m};
