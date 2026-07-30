85345095ccd81aca0a97acf014526c311930492a
PushoDev <bethocubans1990@gmail.com>
2026-07-30 17:03:58 -0400

    Mejorando transacciones sections

--- public/build/assets/app-layout-D4lJTmnw.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-li3mj-Ff.js";import{x as s}from"./app-sidebar-layout-FvSKlj8j.js";const x=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{x as A};
