a432a22614847c2eb3ae45331017ac447b637cfa
PushoDev <bethocubans1990@gmail.com>
2026-08-01 12:17:20 -0400

    Arreglo de columnas Transacciones

---
+++ public/build/assets/app-layout-wgylBAey.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-BXQf3iBD.js";import{x as s}from"./app-sidebar-layout-BT3RxnWU.js";const x=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{x as A};
---
+++ public/build/assets/react-DX9eVNBi.js
@@ -0,0 +1,1 @@
+import{m as o}from"./proxy-DdcD1nls.js";const t=o;export{t as m};
