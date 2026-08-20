76f3dae443ff128b3d696f754c3db659185ff77b
PushoDev <bethocubans1990@gmail.com>
2026-08-20 17:08:03 -0400

    Implementar Prorrateo de Costos II & Compiled

---
+++ public/build/assets/app-layout-C0hFw1AZ.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-C7eq6uPi.js";import{s as r}from"./app-sidebar-layout-DoFQVDaF.js";const i=({children:o,breadcrumbs:p,...s})=>t.jsx(r,{breadcrumbs:p,...s,children:o});export{i as A};
---
+++ public/build/assets/react-BawPmPho.js
@@ -0,0 +1,1 @@
+import{m as o}from"./proxy-D3GZF5tg.js";const t=o;export{t as m};
