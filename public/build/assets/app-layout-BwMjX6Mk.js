ec17a0dd0758e3de233d761f5580927a894a1b16
PushoDev <bethocubans1990@gmail.com>
2026-08-22 16:17:17 -0400

    Compiled 22 de agost

---
+++ public/build/assets/app-layout-BwMjX6Mk.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-h72PxYAd.js";import{s as r}from"./app-sidebar-layout-DIZj0ETB.js";const i=({children:o,breadcrumbs:p,...s})=>t.jsx(r,{breadcrumbs:p,...s,children:o});export{i as A};
